07dacdf5484395949dd09c62ba98952c43e48587
buhtelj <andrej.cijan@gmail.com>
2018-02-14 12:31:29 +0100

    naloga narejena

---
+++ src/com/virtualprojectslab/Main.java
@@ -0,0 +1,22 @@
+package com.virtualprojectslab;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myIntvalue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+
+        //System.out.println("myIntValue = " + myIntvalue);
+        //System.out.println("myFloatValue = " + myFloatValue);
+        //System.out.println("myDoubleValue = " + myDoubleValue);
+
+        double myPounds = 200;
+        double myKiloGrams = myPounds * 0.45359237;
+
+        System.out.println(myPounds + " pounds equals " + myKiloGrams + " kilograms.");
+
+
+    }
+}
